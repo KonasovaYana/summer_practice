41c01b2ccdaf7196d59e990bf33e16fa1f874c59
KonasovaYana <konasovay@gmail.com>
2025-06-30 22:55:56 +0300

    ninth_test

--- src/main/java/com/pinterest/testing/pages/HomePage.java
+++ src/main/java/com/pinterest/testing/pages/HomePage.java
@@ -1,6 +1,6 @@
 package com.pinterest.testing.pages;
 import com.pinterest.testing.elements.Button;
-
+import com.pinterest.testing.elements.Input;
 import static com.codeborne.selenide.Selenide.$x;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,4 +20,39 @@ public class HomePage extends BasePage {
         logger.info("Открытие страницы ваших сохраненных идей");
         openAccountPageButton.click();
     }
+    private final Button openFirstPinButton = Button.createByXpath("//div[@data-test-id='pin']//img\n");
+    public void openFirstPin() {
+        logger.info("Выбираем первый пин в ленте");
+        openFirstPinButton.click();
+    }
+    private final Button sharedAccessButton = Button.createByXpath("//button[@aria-label='Отправить']\n");
+    public void sharedAccess() {
+        logger.info("Нажимаем на кнопку 'общий доступ'");
+        sharedAccessButton.click();
+    }
+
+    private final Input userName = Input.createByXpath("//input[@aria-label='Поле поиска']\n");
+    public void writeUserName(String title) {
+        logger.info("Вводим имя пользователя");
+        userName.setValue(title);
+    }
+
+    private final Button sendPinButton = Button.createByXpath("//button[@data-test-id='sharesheet-contact-send-button']\n");
+    public void sendPin() {
+        logger.info("Отправляем пин выбранному пользователю");
+        sendPinButton.click();
+    }
+
+    private final Button openChatButton = Button.createByXpath("//button[@data-test-id='sharesheet-viewchat-button']\n");
+
+    public boolean checkMessage(){
+        logger.info("Проверяем, отправилось ли сообщение");
+        return openChatButton.isDisplayed();
+    }
+    public void openChat() {
+        logger.info("Переходим в чат с пользователем");
+        openChatButton.click();
+        com.codeborne.selenide.Selenide.sleep(2000);
+    }
+
 }
