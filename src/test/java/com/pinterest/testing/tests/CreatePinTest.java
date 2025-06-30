f690ca70653b8d27d1d22c417e4cd3bead1ee88b
KonasovaYana <konasovay@gmail.com>
2025-06-30 14:09:16 +0300

    first_test_update

--- src/test/java/com/pinterest/testing/tests/CreatePinTest.java
+++ src/test/java/com/pinterest/testing/tests/CreatePinTest.java
@@ -1,24 +1,40 @@
 package com.pinterest.testing.tests;
 
 import com.pinterest.testing.BaseTest;
-import com.pinterest.testing.pages.AccountPage;
 import com.pinterest.testing.pages.HomePage;
 import com.pinterest.testing.pages.LoginPage;
 import com.pinterest.testing.pages.PinCreationPage;
 import org.junit.jupiter.api.Test;
 
-public class CreatePinTest extends BaseTest {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+public class CreatePinTest extends BaseTest {
+    private String getProperty(String key) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("account.properties")) {
+            Properties props = new Properties();
+            if (input == null) {
+                throw new RuntimeException("account.properties not found");
+            }
+            props.load(input);
+            return props.getProperty(key);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load account properties", e);
+        }
+    }
     @Test
+
     public void testCreatePin() {
-        new LoginPage().login("sellithu@ablyd.com", "Test!12345");
+        String email = getProperty("email");
+        String password = getProperty("password");
+        new LoginPage().login(email, password);
         HomePage home = new HomePage();
         home.openPinCreation();
         PinCreationPage creationPage = new PinCreationPage();
         creationPage.uploadImage("flower.jpg");
         creationPage.setTitle("Тестовый пин");
         creationPage.publish();
-
         creationPage.goToPublishedPin();
 
     }
