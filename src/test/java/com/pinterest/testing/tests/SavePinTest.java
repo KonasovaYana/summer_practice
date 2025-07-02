74d19c919767625c53c9fa6a6afe7fedc9f3c34e
3384KhalzevAlexandr <axalzev@inbox.ru>
2025-07-02 16:02:41 +0300

    second_test_update

--- src/test/java/com/pinterest/testing/tests/SavePinTest.java
+++ src/test/java/com/pinterest/testing/tests/SavePinTest.java
@@ -1,4 +1,32 @@
 package com.pinterest.testing.tests;
 
-public class SavePinTest {
+import com.pinterest.testing.BaseTest;
+import com.pinterest.testing.pages.AccountPage;
+import com.pinterest.testing.pages.HomePage;
+import com.pinterest.testing.pages.LoginPage;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+public class SavePinTeat extends BaseTest {
+    private String getProperty(String key) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("account.properties")) {
+            Properties props = new Properties();
+            if (input == null) {
+                throw new RuntimeException("account.properties not found");
+            }
+            props.load(input);
+            return props.getProperty(key);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load account properties", e);
+        }
+    }
+    @Test
+    public void testFavoritePin() {
+        String email = getProperty("email");
+        String password = getProperty("password");
+        new LoginPage().login(email, password);
+        HomePage home = new HomePage();
+    }
 }
