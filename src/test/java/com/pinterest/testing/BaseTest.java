3176f7d9abd98d5ef4a8b1526a60707cf8138aea
3384KhalzevAlexandr <axalzev@inbox.ru>
2025-07-04 16:50:00 +0300

    comments

--- src/test/java/com/pinterest/testing/BaseTest.java
+++ src/test/java/com/pinterest/testing/BaseTest.java
@@ -13,7 +13,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * Базовый класс для всех тестовых классов.
+ * Предоставляет общую конфигурацию браузера и методы для работы с тестами.
+ * Содержит:
+ * - Настройки браузера перед каждым тестом
+ * - Методы закрытия браузера после теста
+ * - Вспомогательные методы для работы с properties-файлами
+ */
 public class BaseTest {
+    /**
+     * Метод выполняемый перед каждым тестом.
+     * Настраивает и открывает браузер с заданными параметрами:
+     * - Устанавливает размер окна браузера
+     * - Открывает стартовую страницу Pinterest
+     */
     @BeforeEach
     public void openBrowser() {
         Configuration.browserSize = "1920x1080";
@@ -24,11 +38,23 @@ public class BaseTest {
         Configuration.browserCapabilities = options;
         open("https://www.pinterest.com/");
     }
+
+    /**
+     * Метод выполняемый после каждого теста.
+     * Закрывает браузер и освобождает ресурсы.
+     */
     @AfterEach
     public void closeBrowser() {
         closeWebDriver();
     }
 
+    /**
+     * Получает значение свойства из файла account.properties.
+     *
+     * @param key ключ свойства для получения
+     * @return значение свойства
+     * @throws RuntimeException если файл не найден или произошла ошибка чтения
+     */
     protected String getProperty(String key) {
         try (InputStream input = getClass().getClassLoader().getResourceAsStream("account.properties")) {
             Properties props = new Properties();
